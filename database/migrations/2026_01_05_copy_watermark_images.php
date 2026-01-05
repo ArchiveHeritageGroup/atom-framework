7831f5b879a342f45e3ed7d862937a9511e2c51d
Your Name <you@example.com>
2026-01-05 08:44:02 +0200

    v1.1.95: Add migration to copy watermark images

---
+++ database/migrations/2026_01_05_copy_watermark_images.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Migration: Copy watermark images to images/watermarks
+ * Date: 2026-01-05
+ */
+
+return new class {
+    public function up(): void
+    {
+        $atomRoot = sfConfig::get('sf_root_dir');
+        $frameworkPath = $atomRoot . '/atom-framework';
+        $sourceDir = $frameworkPath . '/dist/images/watermarks';
+        $targetDir = $atomRoot . '/images/watermarks';
+        
+        if (!is_dir($sourceDir)) {
+            echo "  Source watermark images not found at: $sourceDir\n";
+            return;
+        }
+        
+        if (!is_dir($targetDir)) {
+            mkdir($targetDir, 0755, true);
+        }
+        
+        $files = glob($sourceDir . '/*.png');
+        $copied = 0;
+        
+        foreach ($files as $file) {
+            $basename = basename($file);
+            $target = $targetDir . '/' . $basename;
+            if (copy($file, $target)) {
+                $copied++;
+            }
+        }
+        
+        // Set permissions
+        if (function_exists('posix_getpwnam')) {
+            $www = posix_getpwnam('www-data');
+            if ($www) {
+                chown($targetDir, $www['uid']);
+                chgrp($targetDir, $www['gid']);
+                foreach (glob($targetDir . '/*') as $f) {
+                    chown($f, $www['uid']);
+                    chgrp($f, $www['gid']);
+                }
+            }
+        }
+        
+        echo "  Copied $copied watermark images to $targetDir\n";
+    }
+    
+    public function down(): void
+    {
+        // Don't remove images on rollback
+    }
+};
